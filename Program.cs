3c86eca0e7c92c9eb093b52c74447b83df9d1a37
Parker Hodge <iparkerhodge@gmail.com>
2020-06-01 10:32:59 -0500

    completed phase one

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
 
-namespace bank_heist
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace bank_heist {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Plan your heist!");
+
+            Console.WriteLine ("Enter name");
+            string name = Console.ReadLine ();
+
+            int skillLevel = 1;
+            while (true) {
+                Console.WriteLine ("Enter skill level");
+                bool success = int.TryParse (Console.ReadLine (), out skillLevel);
+
+                if (success && skillLevel > 0) {
+                    Console.WriteLine ($"Skill level set to: {skillLevel}");
+                    break;
+                } else {
+                    Console.WriteLine ("Please enter an integer.");
+                }
+            }
+
+            double courageFactor = 1.0;
+            while (true) {
+                Console.WriteLine ("Please enter courage factor (0.0 - 2.0)");
+                bool success = double.TryParse (Console.ReadLine (), out courageFactor);
+                if (success && courageFactor > 0 && courageFactor < 2.0) {
+                    Console.WriteLine ($"Courage Factor set to: {courageFactor}");
+                    break;
+                } else {
+                    Console.WriteLine ("Must enter value between 0.0 and 2.0");
+                }
+            }
+
+            TeamMember One = new TeamMember (name, skillLevel, courageFactor);
+
+            Console.WriteLine (One);
+
         }
     }
 }
---
+++ TeamMember.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace bank_heist {
+    public class TeamMember {
+        private string _name;
+
+        private int _skillLevel;
+
+        private double _courageFactor;
+
+        public TeamMember (string name, int skill, double courage) {
+            this._name = name;
+            this._skillLevel = skill;
+            this._courageFactor = courage;
+        }
+
+        public override string ToString () {
+            return $@"
+            Name: {_name}
+            Skill Level: {_skillLevel}
+            Courage Factor: {_courageFactor}
+            ";
+        }
+
+    }
+}
